25db2e29a4747d3da61ae274bd169fa14c2acf20
Alkis Gotovos <alkisg@inf.ethz.ch>
2014-10-02 11:53:47 +0200

    Add a bunch of SNAP_GR results

--- code/suite.sh
+++ code/suite.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-python influence.py -m gr -n 1000 -nn 10000 -na 1000 -ni 100 -p 0.1 -k 2 -g 1 -w 4
+python influence.py -m gr -n 1000 -nn 10000 -na 1000 -ni 100 -p 0.01 -k 10 -g 1 -w 3
+python influence.py -m gr -n 1000 -nn 10000 -na 1000 -ni 100 -p 0.05 -k 10 -g 1 -w 3
+python influence.py -m gr -n 1000 -nn 10000 -na 1000 -ni 100 -p 0.1 -k 10 -g 1 -w 3
+python influence.py -m gr -n 1000 -nn 10000 -na 1000 -ni 100 -p 0.15 -k 10 -g 1 -w 3
+python influence.py -m gr -n 1000 -nn 10000 -na 1000 -ni 100 -p 0.2 -k 10 -g 1 -w 3
+python influence.py -m gr -n 1000 -nn 10000 -na 1000 -ni 100 -p 0.3 -k 10 -g 1 -w 3
+python influence.py -m gr -n 1000 -nn 10000 -na 1000 -ni 100 -p 0.4 -k 10 -g 1 -w 3
+python influence.py -m gr -n 1000 -nn 10000 -na 1000 -ni 100 -p 0.5 -k 10 -g 1 -w 3
+python influence.py -m gr -n 1000 -nn 10000 -na 1000 -ni 100 -p 0.6 -k 10 -g 1 -w 3
