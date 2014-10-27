22a100708f11e3478c2f1254355120bb58843ba4
Alkis Gotovos <alkisg@inf.ethz.ch>
2014-10-27 10:43:01 +0100

    Add plot of selected nodes; some new SNAP_GR results

--- code/makeplots.py
+++ code/makeplots.py
@@ -9,6 +9,7 @@ DIR_DATA = '../results'
 DIR_TEMPLATE = '../templates'
 FN_FAVG = 'f_avg.tex'
 FN_FRATIO = 'f_ratio.tex'
+FN_FNODES = 'f_nodes.tex'
 
 def get_coords(x, y):
     s = ''
@@ -35,7 +36,7 @@ def maketex(datadir, fname):
 def clean(datadir):
     cwd = os.getcwd()
     os.chdir(datadir)
-    subprocess.call('rm -f *.tex *.pdf', shell=True)
+    subprocess.call('rm -f *.tex', shell=True)
     os.chdir(cwd)
 
 def process_dir(datadir):
@@ -43,28 +44,38 @@ def process_dir(datadir):
     f_nonad = []
     f_ad = []
     f_ratio = []
+    f_nodes_nonad = []
+    f_nodes_ad = []
     
     fnames = [s for s in os.listdir(datadir) if s.endswith('pickle')]
 
     for fname in fnames:
         with open(os.path.join(datadir, fname)) as f:
+            print 'Processing', fname
             data = pcl.load(f)
             n = 0.01*data['vcount']
             pedge.append(data['pedge'])
             f_nonad.append(np.mean(data['r_nonad_rg']))
             f_ad.append(np.mean(data['r_ad']))
             f_ratio.append((np.mean(data['r_ad'])-np.mean(data['r_nonad_rg']))/n)
-            
+            print data['pedge'], ',', data['v_nonad_rg']
+            f_nodes_nonad.append(np.mean(len(data['v_nonad_rg']))/n)
+            f_nodes_ad.append(np.mean(data['v_ad'])/n)
             
     s_nonad = get_coords(pedge, f_nonad)
     s_ad = get_coords(pedge, f_ad)
     s_ratio = get_coords(pedge, f_ratio)
+    s_nodes_nonad = get_coords(pedge, f_nodes_nonad)
+    s_nodes_ad = get_coords(pedge, f_nodes_ad)
             
     replace(datadir, FN_FAVG, {'f_avg_nonad': s_nonad, 'f_avg_ad': s_ad})
     replace(datadir, FN_FRATIO, {'f_ratio': s_ratio})
+    replace(datadir, FN_FNODES, {'f_nodes_nonad': s_nodes_nonad,
+                                 'f_nodes_ad': s_nodes_ad})
 
     maketex(datadir, FN_FAVG)
     maketex(datadir, FN_FRATIO)
+    maketex(datadir, FN_FNODES)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
